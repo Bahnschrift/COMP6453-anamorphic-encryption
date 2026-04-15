8c8c459b1b35cf59726603684328fddd6eb1baab
Bahnschrift <kobeckett+git@gmail.com>
2026-04-15 18:47:27 +1000

    Made groups make more sense

--- src/el_gamal.rs
+++ src/el_gamal.rs
@@ -2,19 +2,19 @@ use crypto_bigint::Uint;
 use rand::{RngExt, SeedableRng, rngs::ChaCha20Rng};
 
 use crate::{
-    groups::{MGParams, MGVal},
+    groups::MCG,
     helpers::random_mod_lb,
     pke::{AnamorphicPKE, PKE},
 };
 
 /// ElGamal encryption is defined over some group G
 #[derive(Debug)]
-pub struct ElGamal<const LIMBS: usize, G: MGParams<LIMBS>> {
+pub struct ElGamal<const LIMBS: usize, G: MCG<LIMBS>> {
     rng: ChaCha20Rng,
     params: std::marker::PhantomData<G>,
 }
 
-impl<const LIMBS: usize, G: MGParams<LIMBS>> ElGamal<LIMBS, G> {
+impl<const LIMBS: usize, G: MCG<LIMBS>> ElGamal<LIMBS, G> {
     pub fn new() -> Self {
         Self::new_seeded(rand::rng().random())
     }
@@ -27,20 +27,17 @@ impl<const LIMBS: usize, G: MGParams<LIMBS>> ElGamal<LIMBS, G> {
     }
 }
 
-impl<const LIMBS: usize, G: MGParams<LIMBS>> PKE for ElGamal<LIMBS, G> {
-    type PK = MGVal<LIMBS, G>;
-
+impl<const LIMBS: usize, G: MCG<LIMBS>> PKE for ElGamal<LIMBS, G> {
+    type PK = G;
     type SK = Uint<LIMBS>;
-
-    type M = MGVal<LIMBS, G>;
-
-    type C = (MGVal<LIMBS, G>, MGVal<LIMBS, G>);
+    type M = G;
+    type C = (G, G);
 
     fn r#gen(&mut self) -> (Self::PK, Self::SK) {
         // sk = 0 is bad, because then c1 = m
         let sk = random_mod_lb(&mut self.rng, Uint::ONE, G::q());
         // This won't panic, since g^q is by definition in the group
-        let pk = MGVal::into_group(G::g().pow(&sk)).unwrap();
+        let pk = G::into_group(G::g().pow(&sk)).unwrap();
 
         (pk, sk)
     }
@@ -50,16 +47,16 @@ impl<const LIMBS: usize, G: MGParams<LIMBS>> PKE for ElGamal<LIMBS, G> {
         let r = random_mod_lb(&mut self.rng, Uint::ONE, G::q());
 
         // This won't panic, since g^k * (g^a)^r = g^(k + a * r), which is in the group
-        let c1 = MGVal::into_group(*m * pk.pow(&r)).unwrap();
+        let c1 = G::into_group(*m * pk.pow(&r)).unwrap();
         // This won't panic, since this is the definition of being in the group
-        let c2 = MGVal::into_group(G::g().pow(&r)).unwrap();
+        let c2 = G::into_group(G::g().pow(&r)).unwrap();
 
         (c1, c2)
     }
 
     fn dec(&mut self, (c1, c2): Self::C, sk: Self::SK) -> Self::M {
-        MGVal::into_group(*c1 * c2.pow(&sk).invert().unwrap()).unwrap()
+        G::into_group(*c1 * c2.pow(&sk).invert().unwrap()).unwrap()
     }
 }
 
-impl<const LIMBS: usize, G: MGParams<LIMBS>> AnamorphicPKE for ElGamal<LIMBS, G> {}
+impl<const LIMBS: usize, G: MCG<LIMBS>> AnamorphicPKE for ElGamal<LIMBS, G> {}
